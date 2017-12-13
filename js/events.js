8d69a6b397682fa83b17208894f5fa0b3a5683e9
Matthew Dworkin <mdworkin21@gmail.com>
2017-12-13 22:31:42 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,14 @@
 //define functions here
 
+function getIt(){
+  $('p').on('click', function(){
+    alert("Clicked!")
+  })
+}
+
+
 $(document).ready(function(){
 
 // call functions here
-
+getIt()
 });
